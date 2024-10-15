4150f273201b76c5c71f34cef1ce977d20435901
seungwoo <swbaik01@naver.com>
2024-10-15 12:15:35 +0900

    initial commit

---
+++ src/main/java/itm/oss/Main.java
@@ -0,0 +1,11 @@
+package itm.oss;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Calculator cl = new Calculator();
+
+        System.out.println("2 + 3 = "+cl.add(2, 3));
+    }
+}
